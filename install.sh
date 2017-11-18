2008bf72890a1a26bd1197f1d61203f94c2f8946
LinJoseph <lyz2356002@gmail.com>
2017-11-18 21:12:37 +0800

    update my setting

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -f ~/.vimrc
-
-ln _vimrc ~/.vimrc
--- win_install.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-mklink /D %HOMEDRIVE%\%HOMEPATH%\vimfiles %~dp0
-mklink %HOMEDRIVE%\%HOMEPATH%\_vimrc %~dp0\_vimrc
-
-pause
